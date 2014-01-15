0e8b8df719a429855e9edc7d5d11cd50d9842648
Francois Doray <francois.pierre-doray@polymtl.ca>
2014-01-15 16:48:36 -0500

    Debut du prototype handDetector

---
+++ Prototype/handDetector/Assets/HandColor/HandColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Kinect;
+
+public class HandColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Manager manager = Manager.Instance;
+        if (manager == null)
+            return;
+
+        short[] depthStream = manager.GetUsersDepthMap();
+	}
+}
---
+++ Prototype/handDetector/Assets/Kinect/WrapperManaged.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kinect.Managed
+{
+    class DepthImageFrame
+    {
+        public const int PlayerIndexBitMask = 7;
+        public const int PlayerIndexBitMaskWidth = 3;
+    }
+}
