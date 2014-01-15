f7b25e8cfe0cf14acc598d499fd93e56a34a1719
Simon <simon.delisle@polymtl.ca>
2014-01-15 15:50:56 -0500

    Transfere de git

---
+++ Prototype/kinectPiano/Assets/Scripts/Note.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Note : MonoBehaviour {
+
+	public float ecartOctave = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	if(Input.GetButton(key)){
+	//		Debug.Log ("Key pressed!");
+	//	}
+	}
+
+	void OnMouseDown()
+	{
+		audio.pitch = Mathf.Pow(2.0f, ecartOctave);
+		audio.Play();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		audio.pitch = Mathf.Pow(2.0f, ecartOctave);
+		audio.Play();
+		Debug.Log (ecartOctave);
+	}
+}
