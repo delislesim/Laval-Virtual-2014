2c311af7cffa737c5e35459efabfded8133f72c0
Felix <felix.gingras-harvey@polymtl.ca>
2014-01-16 15:14:03 -0500

    #3 Ajout de sons on Click Ajout d'un petit script de debug pour les collisions avec souris. Ajout du snare.blend

--- Prototype/basicPiano/Assets/Scripts/DrumComponent.cs
+++ Prototype/basicPiano/Assets/Scripts/DrumComponent.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class DrumComponent : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
 	
+	void OnMouseDown()
+	{
+		audio.Play();
+		Debug.Log ("YOLOS");
+	}
+	
+	void OnMouseUp()
+	{
+		//audio.Stop();
 	}
 }
