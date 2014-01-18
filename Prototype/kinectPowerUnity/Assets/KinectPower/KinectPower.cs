e3af60f2b24cc6592bc8c2b030e26324d9e98ce3
Francois Doray <fdoray@chromium.org>
2014-01-18 16:46:30 -0500

    Creation d'un projet Unity qui utiliser KinectPower

---
+++ Prototype/kinectPowerUnity/Assets/KinectPower/KinectPower.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinectPower : MonoBehaviour {
+
+  public bool showDepthMap;
+
+	// Use this for initialization
+	void Start () {
+    KinectPowerInterop.Initialize();
+
+    depthTexture = new Texture2D(kDepthWidth, kDepthHeight);
+    depthMapRect = new Rect(Screen.width, Screen.height - depthTexture.height,
+                            -depthTexture.width, depthTexture.height);
+	}
+	
+	void Update () {
+    KinectPowerInterop.GetNiceDepthMap(depthBuffer, (uint)depthBuffer.Length);
+    for (int i = 0; i < depthColors.Length; ++i) {
+      int redIndex = i*4 + 2;
+      int greenIndex = i*4 + 1;
+      int blueIndex = i*4 + 0;
+      int alphaIndex = i*4 + 3;
+
+      depthColors[i] = new Color32(depthBuffer[redIndex],
+                                   depthBuffer[greenIndex],
+                                   depthBuffer[blueIndex],
+                                   depthBuffer[alphaIndex]);
+    }
+
+    depthTexture.SetPixels32(depthColors);
+    depthTexture.Apply();
+	}
+
+  void OnGUI()
+  {
+    if (showDepthMap)
+      GUI.DrawTexture(depthMapRect, depthTexture);
+  }
+
+  void OnDestroy () {
+    KinectPowerInterop.Shutdown();
+  }
+
+  // Depth stream.
+  private Rect depthMapRect;
+  private Texture2D depthTexture;
+  private byte[] depthBuffer = new byte[kDepthWidth * kDepthHeight * 4];
+  private Color32[] depthColors = new Color32[kDepthWidth * kDepthHeight];
+
+  private const int kDepthWidth = 640;
+  private const int kDepthHeight = 480;
+
+}
---
+++ Prototype/kinectPowerUnity/Assets/KinectPower/KinectPowerInterop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class KinectPowerInterop {
+
+  [DllImport(@"kinect_power.dll", EntryPoint = "Initialize", CallingConvention = CallingConvention.Cdecl)]
+  public static extern bool Initialize();
+
+  [DllImport(@"kinect_power.dll", EntryPoint = "Shutdown", CallingConvention = CallingConvention.Cdecl)]
+  public static extern bool Shutdown();
+
+  [DllImport(@"kinect_power.dll", EntryPoint = "GetNiceDepthMap", CallingConvention = CallingConvention.Cdecl)]
+  public static extern bool GetNiceDepthMap(byte[] buffer, uint buffer_size);
+
+}
