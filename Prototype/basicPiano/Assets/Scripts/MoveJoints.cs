ea7efa4194c9626d0061f1c724fbd416aa3a6394
Felix <felix.gingras-harvey@polymtl.ca>
2014-01-25 15:20:33 -0500

    #9 Ajout du script move joints

---
+++ Prototype/basicPiano/Assets/Scripts/MoveJoints.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveJoints : MonoBehaviour {
+
+	public GameObject mainDroite;
+	public GameObject mainGauche;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float[] positions = new float[(int)KinectPowerInterop.NuiSkeletonPositionIndex.Count * 3] ;
+		KinectPowerInterop.GetJointsPosition(0, positions);
+	
+		Vector3 mdPos = new Vector3(positions[(int)KinectPowerInterop.NuiSkeletonPositionIndex.HandRight],
+		                                      positions[(int)KinectPowerInterop.NuiSkeletonPositionIndex.HandRight+1],
+		                                      positions[(int)KinectPowerInterop.NuiSkeletonPositionIndex.HandRight+2]);
+
+		mainDroite.transform.position = mdPos*100;
+	}
+}
