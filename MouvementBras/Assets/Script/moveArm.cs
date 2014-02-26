fccba4eada0a6b7018371fc102d6ea127778175d
Simon <delislesim@gmail.com>
2014-02-26 16:59:40 -0500

    Plein de joint et un bras superbe #3dsmax #joint

---
+++ MouvementBras/Assets/Script/moveArm.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveArm : MonoBehaviour {
+	public GameObject epaule;
+	public GameObject main;
+	public GameObject coude;
+	public GameObject avantBras;
+	public GameObject bicep;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Debug.Log("Application de la force");
+		    main.rigidbody.AddForce (main.transform.up * 1000, ForceMode.Acceleration);
+		}
+	}
+}
