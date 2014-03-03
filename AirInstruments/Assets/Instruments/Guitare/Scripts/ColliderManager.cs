9c92fc03aaf2efddad045b7332048b5ad1f149f2
Felix <c212.felixh@gmail.com>
2014-03-03 18:28:37 -0500

    Joueur de guitare avec squelette. Ajout de script de chose.

---
+++ AirInstruments/Assets/Instruments/Guitare/Scripts/ColliderManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderManager : MonoBehaviour {
+
+	//rightHandPos
+	public Transform RightHand;
+
+	//Keep track of right hand speed
+	private float[] last_positions;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ AirInstruments/Assets/Instruments/Piano/Hands/SkeletonDrawer.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using KinectHelpers;
+
+public class SkeletonDrawer : MonoBehaviour {
+	// Prefab de cylindre.
+	public GameObject cylindrePrefab;
+
+	// Joints
+	public List<GameObject> spine_spheres = new List<GameObject>();
+	public List<GameObject> left_arm_spheres = new List<GameObject>();
+	public List<GameObject> right_arm_spheres = new List<GameObject>();
+	public List<GameObject> left_leg_spheres = new List<GameObject>();
+	public List<GameObject> right_leg_spheres = new List<GameObject>();
+	
+	// Liste des cylindres affiches dans la scene.
+	private List<GameObject> cylindres = new List<GameObject> ();
+	
+	// Nombre de boules rouges a créer.
+	private const int NB_SPHERES = (int)Skeleton.Joint.Count;
+	
+	// Nombre de cylindre a créer par main.
+	private const int NB_CYLINDRES = 19;
+
+
+	// Use this for initialization
+	void Start () {
+		// Creer les cylindres.
+		for (int i = 0; i < NB_CYLINDRES; ++i) {
+			GameObject cylindre = (GameObject)Instantiate (cylindrePrefab);
+			cylindre.transform.parent = this.transform;
+			cylindre.transform.localScale = cylindrePrefab.transform.localScale;
+			cylindres.Add (cylindre);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlacerCylindres();
+	}
+
+
+	void PlacerCylindres()
+	{
+		int i = 0;
+		int boneIdx = 0;
+
+		//Spine
+		for(i = 1 ; i < spine_spheres.Count ; i++)
+		{	
+			if (spine_spheres[i-1]!=null && spine_spheres[i]!=null) {
+				HandCylinder handCylinder = (HandCylinder)cylindres[boneIdx].GetComponent (typeof(HandCylinder));
+				handCylinder.DefinirExtremites (spine_spheres[i-1].transform.position,
+				                                spine_spheres[i].transform.position);
+				cylindres[boneIdx].SetActive(true);
+			} else {
+				cylindres[boneIdx].SetActive(false);
+			}
+			boneIdx++;
+		}
+		//LeftArm
+		for(i = 1 ; i < left_arm_spheres.Count ; i++)
+		{	
+			if (left_arm_spheres[i-1]!=null && left_arm_spheres[i]!=null) {
+				HandCylinder handCylinder = (HandCylinder)cylindres[boneIdx].GetComponent (typeof(HandCylinder));
+				handCylinder.DefinirExtremites (left_arm_spheres[i-1].transform.position,
+				                                left_arm_spheres[i].transform.position);
+				cylindres[boneIdx].SetActive(true);
+			} else {
+				cylindres[boneIdx].SetActive(false);
+			}
+			boneIdx++;
+		}
+
+		//RightArm
+		for(i = 1 ; i < right_arm_spheres.Count ; i++)
+		{	
+			if (right_arm_spheres[i-1]!=null && right_arm_spheres[i]!=null) {
+				HandCylinder handCylinder = (HandCylinder)cylindres[boneIdx].GetComponent (typeof(HandCylinder));
+				handCylinder.DefinirExtremites (right_arm_spheres[i-1].transform.position,
+				                                right_arm_spheres[i].transform.position);
+				cylindres[boneIdx].SetActive(true);
+			} else {
+				cylindres[boneIdx].SetActive(false);
+			}
+			boneIdx++;
+		}
+
+		//LeftLeg
+		for(i = 1 ; i < left_leg_spheres.Count ; i++)
+		{	
+			if (left_leg_spheres[i-1]!=null && left_leg_spheres[i]!=null) {
+				HandCylinder handCylinder = (HandCylinder)cylindres[boneIdx].GetComponent (typeof(HandCylinder));
+				handCylinder.DefinirExtremites (left_leg_spheres[i-1].transform.position,
+				                                left_leg_spheres[i].transform.position);
+				cylindres[boneIdx].SetActive(true);
+			} else {
+				cylindres[boneIdx].SetActive(false);
+			}
+			boneIdx++;
+		}
+
+		//RightLeg
+		for(i = 1 ; i < spine_spheres.Count ; i++)
+		{	
+			if (right_leg_spheres[i-1]!=null && right_leg_spheres[i]!=null) {
+				HandCylinder handCylinder = (HandCylinder)cylindres[boneIdx].GetComponent (typeof(HandCylinder));
+				handCylinder.DefinirExtremites (right_leg_spheres[i-1].transform.position,
+				                                right_leg_spheres[i].transform.position);
+				cylindres[boneIdx].SetActive(true);
+			} else {
+				cylindres[boneIdx].SetActive(false);
+			}
+			boneIdx++;
+		}
+
+
+	}
+}
