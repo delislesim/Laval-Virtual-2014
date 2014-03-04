950c218c12840c8b2e407b090bfa8bf8bb30df3b
Felix <c212.felixh@gmail.com>
2014-03-03 21:44:08 -0500

    Juis un badass qui commit la scène.

--- AirInstruments/Assets/Instruments/Guitare/Scripts/ColliderManager.cs
+++ AirInstruments/Assets/Instruments/Guitare/Scripts/ColliderManager.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class ColliderManager : MonoBehaviour {
 
@@ -7,15 +7,27 @@ public class ColliderManager : MonoBehaviour {
 	public Transform RightHand;
 
 	//Keep track of right hand speed
-	private float[] last_positions;
+	private Queue<Vector3> last_positions;
+
+	private BoxCollider box_collider;
 
 	// Use this for initialization
 	void Start () {
-	
+		last_positions = new Queue<Vector3>();
+		last_positions.Enqueue(new Vector3(0.0f, 0.0f, 0.0f));
+		last_positions.Enqueue(new Vector3(0.0f, 0.0f, 0.0f));
+		//HandCylinder handCylinder = (HandCylinder)cylindres [index].GetComponent (typeof(HandCylinder));
+		box_collider = (BoxCollider)this.GetComponent(typeof(BoxCollider));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		/*Vector3 speed = RightHand.position - last_positions.Dequeue();
+		last_positions.Enqueue(RightHand.position);
+
+		if(box_collider != null){
+			box_collider.transform.localScale = new Vector3(box_collider.transform.localScale.x, speed.magnitude, box_collider.transform.localScale.z);
+			Debug.Log ("Speed :  " + speed);
+		}*/
 	}
 }
