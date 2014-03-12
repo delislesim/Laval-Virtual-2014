cb44268079e4dbfe84346f983e2dc6d992092ef9
Francois Doray <fdoray@chromium.org>
2014-03-11 22:18:22 -0400

    D4: Feedback visuel pour savoir quel instrument on va atteindre (tentative)

---
+++ AirInstruments/Assets/Instruments/Drum/Scripts/BaguetteProjector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaguetteProjector : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = kRotationWorld;
+	}
+
+	// Quaternion de la rotation des projecteur par rapport au monde.
+	private Quaternion kRotationWorld = Quaternion.Euler (new Vector3 (90.0f, 0, 0));
+}
